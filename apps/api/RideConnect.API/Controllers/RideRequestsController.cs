7a0e1199a01d25620d53c6061ca73039afa6888c
Krijn Schelvis <krijn.schelvis@gmail.com>
2026-08-11 18:56:43 +0200

    feat: added ride request endpoints

--- apps/api/RideConnect.API/Controllers/RideRequestsController.cs
+++ apps/api/RideConnect.API/Controllers/RideRequestsController.cs
@@ -8,9 +8,62 @@ namespace RideConnect.API.Controllers;
 public class RideRequestsController : ControllerBase
 {
     [Authorize]
-    [HttpGet]
-    public IActionResult Get()
+    [HttpPost]
+    public Task<IActionResult> CreateRideRequest()
     {
-        return Ok();
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpGet]
+    public Task<IActionResult> GetRideRequests()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpGet("{rideRequestId:guid}")]
+    public Task<IActionResult> GetRideRequestById(Guid rideRequestId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpPut("{rideRequestId:guid}")]
+    public Task<IActionResult> UpdateRideRequest(Guid rideRequestId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpDelete("{rideRequestId:guid}")]
+    public Task<IActionResult> DeleteRideRequest(Guid rideRequestId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    //-------------------------------
+    // Participant Endpoints
+    //-------------------------------
+    
+    [Authorize]
+    [HttpPost("{rideRequestId:guid}/participants")]
+    public Task<IActionResult> JoinRide(Guid rideRequestId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpDelete("{rideRequestId:guid}/participants/me")]
+    public Task<IActionResult> LeaveRide(Guid rideRequestId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Authorize]
+    [HttpGet("{rideRequestId:guid}/participants")]
+    public Task<IActionResult> GetRideParticipants(Guid rideRequestId)
+    {
+        throw new NotImplementedException();   
     }
 }
